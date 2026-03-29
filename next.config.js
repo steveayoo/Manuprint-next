a399662cafd43f5937f77112aa8a58f66d988016
steveayoo <101216094+steveayoo@users.noreply.github.com>
2026-03-29 23:03:57 +0300

    Complete Manuprints Next.js website with MongoDB and Cloudinary

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  serverExternalPackages: ["mongoose"],
+};
+
+module.exports = nextConfig;
