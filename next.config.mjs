9e3c5c6d9e72af9b9d00c2587a2cfbf29b49c0b1
Bret <bremmurd08@gmail.com>
2026-03-30 00:53:39 +0300

    Feat:Upgrade Cloudinary to v2 and remove multer packages as they are not used

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  serverExternalPackages: ["mongoose"],
   reactCompiler: true,
 };
 
